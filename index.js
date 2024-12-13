d92bb823d8b6017499e756602e746e1273910efd
ashiku2 <ashikur1602@gmail.com>
2024-12-13 12:23:08 +0600

    ssl ecommerce added

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ require('dotenv').config();
 // middlewares 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const req = require('express/lib/request');
+const { default: axios } = require('axios');
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.bpilnp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,7 +32,7 @@ async function run() {
 
     const serviceCollection = client.db('portfolio').collection('services');
 
-  
+
 
     const testimonialCollection = client.db('portfolio').collection('testimonials');
 
@@ -39,6 +41,7 @@ async function run() {
     const skillRelatedProjectCollection = client.db('portfolio').collection('skillRelatedProjects');
     const stackOverflowCollection = client.db('portfolio').collection('stackOverflows');
     const topProjectCollection = client.db('portfolio').collection('topProjects');
+    const paymentCollection = client.db('portfolio').collection('payments');
 
     // 1. Service api 
 
@@ -105,7 +108,7 @@ async function run() {
     app.put('/testimonial/:id', async (req, res) => {
       const data = req.body;
       const id = req.params.id;
-      const query = { _id : new ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatedInfo = {
         $set: {
@@ -330,6 +333,105 @@ async function run() {
       const result = await topProjectCollection.deleteOne(query);
       res.send(result);
     })
+
+
+    // checkout related api
+
+    app.post("/create-payment", async (req, res) => {
+
+      const data = req.body;
+      const trxId = new ObjectId().toString();
+
+      const initiateData = {
+
+        store_id: 'ashli67571beb67054',
+        store_passwd: 'ashli67571beb67054@ssl',
+        total_amount: data.amount,
+        currency: 'BDT',
+        tran_id: trxId,
+        success_url: 'https://portfolio-server-opal-alpha.vercel.app/success-payment',
+        fail_url: 'https://portfolio-server-opal-alpha.vercel.app/fail',
+        cancel_url: 'https://portfolio-server-opal-alpha.vercel.app/cancel',
+        cus_name: ' Customer Name',
+        cus_email: 'cust @yahoo.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        shipping_method: "NO",
+        product_name: "laptop",
+        product_category: "laptop",
+        product_profile: "general",
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D',
+        multi_card_name: 'mastercard, visacard, amexcard',
+
+
+
+      }
+
+      const response = await axios({
+        method: "POST",
+        url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+        data: initiateData,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+
+
+      const saveData = {
+        cus_name: "Dummy",
+        paymentId: trxId,
+        amount: data.amount,
+        status: "Pending",
+      }
+
+      const save = await paymentCollection.insertOne(saveData);
+      if (save) {
+        res.send({
+          paymentUrl: response.data.GatewayPageURL,
+        });
+      }
+
+
+    })
+
+    app.post('/success-payment', async (req, res) => {
+      const successData = req.body;
+      if (successData.status !== "VALID") {
+        throw new Error("Unauthorized payment, Invalid payment");
+      }
+
+      const query = {
+        paymentId: successData.tran_id
+      }
+      const update = {
+        $set: {
+          status: "Success",
+        },
+      }
+
+      const updateData = await paymentCollection.updateOne(query, update);
+      
+      res.redirect("https://ashikurrahman-33fb8.web.app/success")
+    })
+
+    app.post("/fail", async (req, res) => {
+      res.redirect("https://ashikurrahman-33fb8.web.app/fail")
+    })
+
+    app.post("/cancel", async (req, res) => {
+      res.redirect("https://ashikurrahman-33fb8.web.app/cancel")
+    })
+
+
 
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
